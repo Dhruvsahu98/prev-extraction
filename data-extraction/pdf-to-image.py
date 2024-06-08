0b69d199de5513232f2a8aa3403558bf6cd841c6
Dhruvsahu98 <sahudhruv98@gmail.com>
2024-06-08 23:59:11 +0530

    Added two files

--- data-extraction/pdf-to-image.py
+++ data-extraction/pdf-to-image.py
@@ -18,6 +18,6 @@ def pdf_to_images(pdf_file, output_folder, dpi=300, fmt="jpg", first_page=1, las
         image.save(os.path.join(output_folder, f"page_{i+first_page-1}.{fmt}"), fmt, quality=95)
 
 # Example usage:
-pdf_file = "C:\\Users\\aksha\\Desktop\\MADII\\IITM\\New\\prev-extraction\\data-extraction\\input\\QUIZ-1-QPH1---Jan-2022.pdf"
-output_folder = "C:\\Users\\aksha\\Desktop\\MADII\\IITM\\New\\prev-yrs-data-bs-ds\\MLP\\Jan-2022\\input"
-pdf_to_images(pdf_file, output_folder, dpi=600, fmt="jpeg", first_page=141, last_page=153)  # Convert pages 2-4
+pdf_file = "C:\\Users\\aksha\\Desktop\\MADII\\IITM\\New\\prev-extraction\\data-extraction\\input\\IIT-M-ENDTERM-AUG-2022---MLP.pdf"
+output_folder = "C:\\Users\\aksha\\Desktop\\MADII\\IITM\\New\\prev-yrs-data-bs-ds\\Diploma\\TDS\\Endterm\\AUG-2022\\input"
+pdf_to_images(pdf_file, output_folder, dpi=600, fmt="jpeg", first_page=305, last_page=331)  # Convert pages 2-4
